fe6e159f3df9445e53b8771379509b5c5d9f1107
Stephen Ozan <stephenozan@utexas.edu>
2025-03-25 12:54:59 -0500

    midi input

---
+++ Assets/Scripts/MidiInput.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MidiJack;
+
+public class MidiInput : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+void Update()
+    {
+        for (int note = 0; note < 128; note++)
+        {
+            if (MidiMaster.GetKeyDown(note))
+            {
+                Debug.Log($"MIDI Note Received: {note}");
+            }
+        }
+    }
+}
+    // Update is called once per frame
